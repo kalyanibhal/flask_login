a91fa4fccfaa00a16d7a48b9cd2b5c1ef015d276
danishred <83757129+danishred@users.noreply.github.com>
2024-06-20 22:35:50 +0530

    Version 1.3

--- model/user_model.py
+++ model/user_model.py
@@ -12,59 +12,52 @@ import psycopg2.extras # Allows referencing as dictionary
 from dotenv import load_dotenv
 from flask import jsonify
 
+load_dotenv()
+
 class user_model():
-
-    # Constructor
-    def __init__(self):
-
-        # Loads environment variables
-        load_dotenv()
-
-        # Establishing Connection   
-        url = os.getenv("POSTGRES_URL")
-        try:
-            self.connection = psycopg2.connect(url)
-        except:
-            print("Connection Establishment Failed")
 
     # For registering a new user
     def user_addone_model(self, email, password):    
         # For establishing the connection
-        with self.connection:
-            with self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as self.cursor:
+        url = os.getenv("POSTGRES_URL")
+        connection = psycopg2.connect(url)
+        with connection:
+            with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
 
                 # Check for duplicate email
-                self.cursor.execute("CREATE TABLE IF NOT EXISTS users(email text primary key, password text)")
-                self.cursor.execute("SELECT COUNT(*) FROM users WHERE email = %s", (email,))
+                cursor.execute("CREATE TABLE IF NOT EXISTS users(email text primary key, password text)")
+                cursor.execute("SELECT COUNT(*) FROM users WHERE email = %s", (email,))
                 
                 # Storing first row in count
-                count = self.cursor.fetchone()[0]
+                count = cursor.fetchone()[0]
                 
                 # Checking for email in the database
                 if count > 0:
-                    return jsonify({"Prompt": "Email already taken"}), 409
+                    return jsonify({"Prompt": "Email already taken"})
 
                 # PSQL query to add user to database
-                self.cursor.execute("INSERT INTO users (email, password) Values(%s, %s)",(email, password)) 
-                return jsonify({"Prompt": "User Created Succesfully"}) , 201
+                cursor.execute("INSERT INTO users (email, password) Values(%s, %s)",(email, password)) 
+                return jsonify({"Prompt": "User Created Succesfully"})
 
     # Checking for existing user
     def user_login_model(self, email):
     
-        with self.connection:
-            with self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as self.cursor:
+        url = os.getenv("POSTGRES_URL")
+        connection = psycopg2.connect(url)
+        with connection:
+            with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
 
                 # Check if email exists
-                self.cursor.execute("SELECT COUNT(*) FROM users WHERE email = %s", (email,))
+                cursor.execute("SELECT COUNT(*) FROM users WHERE email = %s", (email,))
 
                 # Storing first row in count
-                count = self.cursor.fetchone()[0]
+                count = cursor.fetchone()[0]
                 
                 # Checking user credentials
                 if count > 0:
-                    return jsonify({"Prompt":"Login Successful"}), 200
+                    return jsonify({"Prompt":"Login Successful"})
                 else:        
-                    return jsonify({"Prompt":"You have entered wrong credentials or user doesn't exists"}), 401
+                    return jsonify({"Prompt":"You have entered wrong credentials or user doesn't exists"})
 
             
                 
